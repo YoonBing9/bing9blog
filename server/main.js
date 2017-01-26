3d9c41bc7f4a04fa3a037afed73cf6ec4e549167
Bing9 <akass191@gmail.com>
2017-01-27 01:36:09 +0900

    first commit

---
+++ server/main.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import path from 'path';
+import morgan from 'morgan'; // HTTP REQUEST LOGGER
+import bodyParser from 'body-parser'; // PARSE HTML BODY
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+
+const app = express();
+const port = 3000;
+const devPort = 4000;
+
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/codelab');
+
+app.use('/', express.static(path.join(__dirname, './../public')));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+/* use session */
+app.use(session({
+    secret: 'Bing9$sarang$191',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.listen(port, () => {
+    console.log('Express is listening on port', port);
+});
+
+if(process.env.NODE_ENV == 'development') {
+    console.log('Server is running on development mode');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listening on port', devPort);
+        }
+    );
+}
